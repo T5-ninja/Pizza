8b137d4e2493856ad292a6d7d89a60b87ffbfeb2
castlemas <71215246+castlemas@users.noreply.github.com>
2022-06-23 13:20:39 +0800

    Update app.R

--- app.R/app.R
+++ app.R/app.R
@@ -6,80 +6,24 @@
 # hi
 #    http://shiny.rstudio.com/
 #
-remotes::install_github("RinteRface/shinyRPG")
+install.packages("shinythemes")
+
+## ui.R ##
 library(shiny)
-library(shinyRPG)
-ui <- rpgPage(
-  rpgContainer(
-    rpgProgress("progress", 10),
-    rpgProgress("progress", 20),
-    rpgButton("update", "Update slider"),
-    rpgSlider("slider", "Slider", 0, 100, 50, golden = TRUE),
-    rpgCheckbox("checkbox", "Check me!"),
-    rpgRadio("radio", "Choose one", names(mtcars)),
-    rpgSelect("variable", "Variable:",
-              c("Cylinders" = "cyl",
-                "Transmission" = "am",
-                "Gears" = "gear")),
-    rpgSelect("variable2", "Variable 2:",
-              c("Cylinders" = "cyl",
-                "Transmission" = "am",
-                "Gears" = "gear"), size = 3),
-    style = "framed-golden",
-    rpgContainer(
-      rpgProgress("progress", 10),
-      rpgProgress("progress", 20),
-      rpgButton("update", "Update slider"),
-      rpgSlider("slider", "Slider", 0, 100, 50, golden = TRUE),
-      rpgCheckbox("checkbox", "Check me!"),
-      rpgRadio("radio", "Choose one", names(mtcars)),
-      rpgSelect("variable", "Variable:",
-                c("Cylinders" = "cyl",
-                  "Transmission" = "am",
-                  "Gears" = "gear")),
-      rpgSelect("variable2", "Variable 2:",
-                c("Cylinders" = "cyl",
-                  "Transmission" = "am",
-                  "Gears" = "gear"), size = 3),
-      style = "framed-golden"
-    )
-  )
-  
+library(bslib)
+library(shinythemes)
+
+shinyApp(
+  ui = navbarPage("United",
+                  theme = shinythemes::shinytheme("united"),
+                  tabPanel("Plot", "Plot tab contents..."),
+                  navbarMenu("More",
+                             tabPanel("Summary", "Summary tab contents..."),
+                             tabPanel("Table", "Table tab contents...")
+                  )
+  ),
 )
+# server = function(input, output)
+# {
 
-server <- function(input, output, session) {
-  observe({
-    print(
-      list(
-        button = input$update,
-        slider = input$slider,
-        checkbox = input$checkbox,
-        select = input$variable,
-        list = input$variable2
-      )
-    )
-  })
-  
-  observeEvent({
-    req(input$update > 0)
-    input$update
-  }, {
-    updateRpgSlider(session, "slider", 50)
-    updateRpgProgress("progress", color = "green")
-    updateRpgSelect(session, inputId = "variable2", selected = "gear")
-  })
-  
-  observeEvent(input$slider, {
-    updateRpgProgress("progress", value = input$slider)
-  })
-  
-  observeEvent(c(input$checkbox, input$radio), {
-    if (input$checkbox) showNotification("Hello")
-    showNotification(input$radio)
-  })
-}
-
-
-
-shinyApp(ui, server)
-
+# }
